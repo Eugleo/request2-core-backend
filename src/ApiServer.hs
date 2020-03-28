8e2fb71fd4b7a701dbbe3ab8c97d832c2e0af985
Mirek Kratochvil <exa.exa@gmail.com>
2020-03-28 19:29:15 +0100

    Allow more CORS request types

--- src/ApiServer.hs
+++ src/ApiServer.hs
@@ -12,24 +12,30 @@ import qualified Data.Text as T
 import Environment
 import Model.User (Role (..))
 import Network.Wai
-import Web.Scotty (delete, get, middleware, notFound, post, put, scotty)
-import qualified Web.Scotty as S (function, json, options, text)
+import Web.Scotty (delete, get, post, put, scotty)
+import qualified Web.Scotty as S
+  ( function,
+    json,
+    middleware,
+    notFound,
+    options,
+    text,
+  )
 
 addCORSHeader :: Middleware
 addCORSHeader =
-  modifyResponse $
-    mapResponseHeaders
-      ( [ ("Access-Control-Allow-Origin", "*"),
-          ("Access-Control-Allow-Headers", "*")
-        ]
-          ++
-      )
+  modifyResponse . mapResponseHeaders $
+    (++)
+      [ ("Access-Control-Allow-Origin", "*"),
+        ("Access-Control-Allow-Headers", "*"),
+        ("Access-Control-Allow-Methods", "*")
+      ]
 
 apiServer :: ServerConfig -> IO ()
 apiServer config =
-  scotty (listenPort config) $ do
-    when (allowCORS config) $ do
-      middleware addCORSHeader
+  scotty (_listenPort config) $ do
+    when (_allowCORS config) $ do
+      S.middleware addCORSHeader
       S.options (S.function $ const $ Just []) $ S.text "CORS OK"
     {-
      - Capabilities
@@ -67,7 +73,7 @@ apiServer config =
     {-
      - Standard 404 -- keep this last
      -}
-    notFound $ S.text "Not found"
+    S.notFound $ S.text "Not found"
   where
     withDB = withDBEnv config
     withAuth = withAuthEnv config
