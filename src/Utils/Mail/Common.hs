013386283bce32b03194676c381ec4bd492732fd
Evžen Wybitul <wybitul.evzen@gmail.com>
2021-01-07 17:08:45 +0100

    Remove typo

--- src/Utils/Mail/Common.hs
+++ src/Utils/Mail/Common.hs
@@ -30,7 +30,7 @@ defaultFromAddr c =
 sendmail' :: Config -> Mail -> IO ()
 sendmail' c m =
     S.renderSendMailCustom
-        "/usr/bin/mail"
+        "/usr/sbin/sendmail"
         ["-t", "-f", T.unpack (_mailEnvelopeFrom c)]
         m
             { M.mailFrom = defaultFromAddr c,
