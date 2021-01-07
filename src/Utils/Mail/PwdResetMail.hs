04c5e2f5dc853088f0cc05a33a7151aaa58c87e0
Evžen Wybitul <wybitul.evzen@gmail.com>
2021-01-07 20:13:33 +0100

    I'm tired

--- src/Utils/Mail/PwdResetMail.hs
+++ src/Utils/Mail/PwdResetMail.hs
@@ -1,6 +1,7 @@
 module Utils.Mail.PwdResetMail where
 
 import qualified Data.Text as T
+import Network.Mail.Mime (ImageContent (ImageFilePath), InlineImage (..))
 import Server.Config
 import Utils.Mail.Common
 
